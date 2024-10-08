e75f90698a96129dcf371e0cde448117f7a0e8b3
Arii <dangvtn2004@gmail.com>
2024-10-08 14:14:51 +0700

    Brand

--- src/main/java/com/emc/belustyle/security/SecurityConfig.java
+++ src/main/java/com/emc/belustyle/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
