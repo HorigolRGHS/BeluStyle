9bcd8f9ef5d4a7333e6432ad8663e0f9a7a23efd
HorigolRGHS <sodoshinochi@gmail.com>
2024-10-13 23:48:17 +0700

    Update view stock list

--- src/main/java/com/emc/belustyle/rest/StockRestController.java
+++ src/main/java/com/emc/belustyle/rest/StockRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/stocks/")
+@RequestMapping("/api/stocks")
 public class StockRestController {
 
     private StockService stockService;
